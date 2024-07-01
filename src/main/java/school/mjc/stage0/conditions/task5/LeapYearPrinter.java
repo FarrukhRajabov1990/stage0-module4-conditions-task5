126b345f251e6d06f94ea1e05ec7d0a076ffd552
Farrukh_Rajabov <Farruhrajabov2012@gmail.com>
2024-07-01 22:56:46 +0500

    Initial commit

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -1,6 +1,22 @@
 package school.mjc.stage0.conditions.task5;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        switch (year) {
+            case 2000:
+            case 2004:
+            case 2008:
+            case 2012:
+            case 2016:
+            case 2020:
+            case 2024:
+                System.out.println("leap");
+                break;
+            default:
+                System.out.println("not leap");
+                break;
+        }
     }
 }
